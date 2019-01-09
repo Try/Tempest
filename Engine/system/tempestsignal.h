4747b2de45fd3a19c235c57d8dde084690b83dd9
Try <try9998@gmail.com>
2019-01-09 22:29:30 +0300

    signals: subscription to base class

--- Engine/system/tempestsignal.h
+++ Engine/system/tempestsignal.h
@@ -17,22 +17,22 @@ class Signal<void(Args...args)> {
     Signal()=default;
     Signal(const Signal&)=delete;
 
-    template<class T,class Ret,class ... TArgs>
-    void bind(T* obj,Ret (T::*fn)(TArgs...a)) {
+    template<class T,class Base,class Ret,class ... TArgs>
+    void bind(T* obj,Ret (Base::*fn)(TArgs...a)) {
       using Check=decltype((std::declval<T>().*fn)(std::declval<Args>()...)); // beautify compiller error message
       static_assert(std::is_same<Check,Ret>::value,""); // unused type warning
-      storage.template push<TImpl<T,Ret,TArgs...>>(obj,fn);
+      storage.template push<TImpl<T,Base,Ret,TArgs...>>(obj,fn);
       }
 
-    template<class T,class Ret,class ... TArgs>
-    void bind(T* obj,Ret (T::*fn)(TArgs...a) const) {
+    template<class T,class Base,class Ret,class ... TArgs>
+    void bind(T* obj,Ret (Base::*fn)(TArgs...a) const) {
       using Check=decltype((std::declval<T>().*fn)(std::declval<Args>()...)); // beautify compiller error message
       static_assert(std::is_same<Check,Ret>::value,""); // unused type warning
-      storage.template push<TImplConst<T,Ret,TArgs...>>(obj,fn);
+      storage.template push<TImplConst<T,Base,Ret,TArgs...>>(obj,fn);
       }
 
-    template<class T,class Ret,class ... TArgs>
-    void ubind(T* obj,Ret (T::*fn)(TArgs...a)) {
+    template<class T,class Base,class Ret,class ... TArgs>
+    void ubind(T* obj,Ret (Base::*fn)(TArgs...a)) {
       auto b = storage.begin();
       auto e = storage.end();
 
@@ -56,10 +56,10 @@ class Signal<void(Args...args)> {
       }
 
   private:
-    template<class T,class Ret,class ... TArgs>
+    template<class T,class Base,class Ret,class ... TArgs>
     struct TImpl;
 
-    template<class T,class Ret,class ... TArgs>
+    template<class T,class Base,class Ret,class ... TArgs>
     struct TImplConst;
 
     struct alignas(std::max_align_t) Impl { // force maximum aligment to emplace Impl's into SignalStorage
@@ -87,12 +87,12 @@ class Signal<void(Args...args)> {
         }
       };
 
-    template<class T,class Ret,class ... TArgs>
+    template<class T,class Base,class Ret,class ... TArgs>
     struct alignas(std::max_align_t) TImpl : Impl {
       T*  obj;
-      Ret (T::*fn)(TArgs...a);
+      Ret (Base::*fn)(TArgs...a);
 
-      TImpl(T* obj,Ret (T::*fn)(TArgs...a)):obj(obj),fn(fn){
+      TImpl(T* obj,Ret (Base::*fn)(TArgs...a)):obj(obj),fn(fn){
         this->vtbl = getVtbl();
         }
 
@@ -121,12 +121,12 @@ class Signal<void(Args...args)> {
         }
       };
 
-    template<class T,class Ret,class ... TArgs>
+    template<class T,class Base,class Ret,class ... TArgs>
     struct alignas(std::max_align_t) TImplConst : Impl {
       T* obj;
-      Ret (T::*fn)(TArgs...a) const;
+      Ret (Base::*fn)(TArgs...a) const;
 
-      TImplConst(T* obj,Ret (T::*fn)(TArgs...a) const):obj(obj),fn(fn){
+      TImplConst(T* obj,Ret (Base::*fn)(TArgs...a) const):obj(obj),fn(fn){
         this->vtbl = getVtbl();
         }
 
