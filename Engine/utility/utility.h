806a8f8b2ba50a88ef6b605d96d99796385e4629
Try <try9998@gmail.com>
2019-09-10 19:27:19 +0200

    + Vec3

--- Engine/utility/utility.h
+++ Engine/utility/utility.h
@@ -81,6 +81,37 @@ class BasicPoint<T,2> {
   };
 
 template<class T>
+class BasicPoint<T,3> {
+  public:
+    BasicPoint()=default;
+    BasicPoint(T x,T y,T z):x(x),y(y),z(z){}
+
+    BasicPoint& operator -= ( const BasicPoint & p ){ x-=p.x; y-=p.y; z-=p.z; return *this; }
+    BasicPoint& operator += ( const BasicPoint & p ){ x+=p.x; y+=p.y; z+=p.z; return *this; }
+
+    BasicPoint& operator /= ( const T& p ){ x/=p; y/=p; z/=p; return *this; }
+    BasicPoint& operator *= ( const T& p ){ x*=p; y*=p; z*=p; return *this; }
+
+    friend BasicPoint operator - ( BasicPoint l,const BasicPoint & r ){ l-=r; return l; }
+    friend BasicPoint operator + ( BasicPoint l,const BasicPoint & r ){ l+=r; return l; }
+
+    friend BasicPoint operator / ( BasicPoint l,const T r ){ l/=r; return l; }
+    friend BasicPoint operator * ( BasicPoint l,const T r ){ l*=r; return l; }
+
+    BasicPoint operator - () const { return BasicPoint(-x,-y,-z); }
+
+    T manhattanLength() const { return std::sqrt(x*x+y*y+z*z); }
+    T quadLength()      const { return x*x+y*y+z*z; }
+
+    bool operator ==( const BasicPoint & other ) const { return x==other.x && y==other.y && z==other.z; }
+    bool operator !=( const BasicPoint & other ) const { return x!=other.x || y!=other.y || z!=other.z; }
+
+    T x=T();
+    T y=T();
+    T z=T();
+  };
+
+template<class T>
 struct BasicSize {
   explicit BasicSize(T is = T()):w(is), h(is) {}
   BasicSize(T ix, T iy):w(ix), h(iy) {}
@@ -149,7 +180,7 @@ using PointF=BasicPoint<float,2>;
 
 using Vec1 =BasicPoint<float,1>;
 using Vec2 =BasicPoint<float,2>;
-//using vec3 =BasicPoint<float,3>;
+using Vec3 =BasicPoint<float,3>;
 
 using Size=BasicSize<int>;
 using Rect=BasicRect<int>;
