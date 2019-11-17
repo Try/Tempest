ee0c527a9ff714fc89c28619e7b6344cfad63932
Try <try9998@gmail.com>
2019-11-17 15:52:20 +0100

    gapi-init test

---
+++ Tests/tests/gpudevice_test.cpp
@@ -0,0 +1,12 @@
+#include <Tempest/VulkanApi>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
+using namespace Tempest;
+
+TEST(main,VulkanApi) {
+  VulkanApi api;
+  (void)api;
+  }
