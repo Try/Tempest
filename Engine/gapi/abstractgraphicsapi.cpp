216980949e9d0b5b7ee209e64dacaaa88a69679d
Try <try9998@gmail.com>
2020-05-28 00:09:46 +0200

    CI

--- Engine/gapi/abstractgraphicsapi.cpp
+++ Engine/gapi/abstractgraphicsapi.cpp
@@ -4,15 +4,15 @@ using namespace Tempest;
 
 bool AbstractGraphicsApi::Props::hasSamplerFormat(TextureFormat f) const {
   uint64_t  m = uint64_t(1) << uint64_t(f);
-  return smpFormat&m;
+  return (smpFormat&m)!=0;
   }
 
 bool AbstractGraphicsApi::Props::hasAttachFormat(TextureFormat f) const {
   uint64_t  m = uint64_t(1) << uint64_t(f);
-  return attFormat&m;
+  return (attFormat&m)!=0;
   }
 
 bool AbstractGraphicsApi::Props::hasDepthFormat(TextureFormat f) const {
   uint64_t  m = uint64_t(1) << uint64_t(f);
-  return dattFormat&m;
+  return (dattFormat&m)!=0;
   }
