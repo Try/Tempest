1d0970c744e8373b75912e42145412ccb04b6285
Try <try9998@gmail.com>
2022-04-10 20:45:03 +0200

    metal rq fixup

--- Engine/gapi/metal/mtaccelerationstructure.mm
+++ Engine/gapi/metal/mtaccelerationstructure.mm
@@ -65,64 +65,76 @@ MtAccelerationStructure::~MtAccelerationStructure() {
 
 MtTopAccelerationStructure::MtTopAccelerationStructure(MtDevice& dx, const RtInstance* inst, AccelerationStructure*const* as, size_t asSize)
   :owner(dx) {
-  instances = [dx.impl newBufferWithLength:sizeof(MTLAccelerationStructureInstanceDescriptor)*asSize
-                                   options:MTLResourceStorageModeManaged];
+  NSMutableArray* asArray = nil;
+  id<MTLBuffer>   scratch = nil;
+  try {
+    instances = [dx.impl newBufferWithLength:sizeof(MTLAccelerationStructureInstanceDescriptor)*asSize
+                                     options:MTLResourceStorageModeManaged];
+    if(instances==nil)
+      throw std::system_error(GraphicsErrc::OutOfVideoMemory);
 
-  for(size_t i=0; i<asSize; ++i) {
-    auto& obj = reinterpret_cast<MTLAccelerationStructureInstanceDescriptor*>([instances contents])[i];
+    asArray = [NSMutableArray arrayWithCapacity:asSize];
+    if(asArray==nil && asSize>0)
+      throw std::system_error(GraphicsErrc::OutOfHostMemory);
 
-    for(int x=0; x<3; ++x)
-      for(int y=0; y<4; ++y)
-        obj.transformationMatrix[x][y] = inst[i].mat.at(y,x);
-    obj.options                         = MTLAccelerationStructureInstanceOptionDisableTriangleCulling |
-                                           MTLAccelerationStructureInstanceOptionOpaque;
-    obj.mask                            = 0xFF;
-    obj.intersectionFunctionTableOffset = 0;
-    obj.accelerationStructureIndex      = i;
+    auto desc = [MTLInstanceAccelerationStructureDescriptor new];
+    if(desc==nil)
+      throw std::system_error(GraphicsErrc::OutOfHostMemory);
+
+    desc.instanceDescriptorBuffer        = instances;
+    desc.instanceDescriptorBufferOffset  = 0;
+    desc.instanceDescriptorStride        = sizeof(MTLAccelerationStructureInstanceDescriptor);
+    desc.instanceCount                   = asSize;
+    desc.instancedAccelerationStructures = asArray;
+
+    MTLAccelerationStructureSizes sz = [dx.impl accelerationStructureSizesWithDescriptor:desc];
+
+    impl = [dx.impl newAccelerationStructureWithSize:sz.accelerationStructureSize];
+    if(impl==nil)
+      throw std::system_error(GraphicsErrc::OutOfVideoMemory);
+
+    for(size_t i=0; i<asSize; ++i) {
+      auto& obj = reinterpret_cast<MTLAccelerationStructureInstanceDescriptor*>([instances contents])[i];
+
+      for(int x=0; x<3; ++x)
+        for(int y=0; y<4; ++y)
+          obj.transformationMatrix[x][y] = inst[i].mat.at(y,x);
+      obj.options                         = MTLAccelerationStructureInstanceOptionDisableTriangleCulling |
+                                            MTLAccelerationStructureInstanceOptionOpaque;
+      obj.mask                            = 0xFF;
+      obj.intersectionFunctionTableOffset = 0;
+      obj.accelerationStructureIndex      = i;
+
+      auto* ax = reinterpret_cast<MtAccelerationStructure*>(as[i]);
+      [asArray addObject:ax->impl];
+      }
+
+    id<MTLBuffer> scratch = [dx.impl newBufferWithLength:sz.buildScratchBufferSize options:MTLResourceStorageModePrivate];
+    if(scratch==nil && sz.buildScratchBufferSize>0)
+      throw std::system_error(GraphicsErrc::OutOfVideoMemory);
+
+    id<MTLCommandBuffer>                       cmd = [dx.queue commandBuffer];
+    id<MTLAccelerationStructureCommandEncoder> enc = [cmd accelerationStructureCommandEncoder];
+    [enc buildAccelerationStructure:impl
+                         descriptor:desc
+                      scratchBuffer:scratch
+                scratchBufferOffset:0];
+    [enc endEncoding];
+    [cmd commit];
+    // TODO: implement proper upload engine
+    [cmd waitUntilCompleted];
     }
-
-  if(instances==nil)
-    throw std::system_error(GraphicsErrc::OutOfVideoMemory);
-
-  NSMutableArray* asArray = [NSMutableArray arrayWithCapacity:asSize];
-  if(instances==nil)
-    throw std::system_error(GraphicsErrc::OutOfHostMemory);
-
-  for(size_t i=0; i<asSize; ++i) {
-    auto* ax = reinterpret_cast<MtAccelerationStructure*>(as[i]);
-    [asArray addObject:ax->impl];
+  catch(...) {
+    if(asArray!=nil)
+      [asArray release];
+    if(scratch!=nil)
+      [scratch release];
+    if(impl!=nil)
+      [impl release];
+    if(instances!=nil)
+      [instances release];
+    throw;
     }
-
-  auto* desc = [MTLInstanceAccelerationStructureDescriptor new];
-  desc.instanceDescriptorBuffer        = instances;
-  desc.instanceDescriptorBufferOffset  = 0;
-  desc.instanceDescriptorStride        = sizeof(MTLAccelerationStructureInstanceDescriptor);
-  desc.instanceCount                   = 1;
-  desc.instancedAccelerationStructures = asArray;
-
-  MTLAccelerationStructureSizes sz = [dx.impl accelerationStructureSizesWithDescriptor:desc];
-
-  impl = [dx.impl newAccelerationStructureWithSize:sz.accelerationStructureSize];
-  if(impl==nil)
-    throw std::system_error(GraphicsErrc::OutOfVideoMemory);
-
-  id<MTLBuffer> scratch = [dx.impl newBufferWithLength:sz.buildScratchBufferSize options:MTLResourceStorageModePrivate];
-  if(scratch==nil && sz.buildScratchBufferSize>0) {
-    [instances release];
-    [impl release];
-    throw std::system_error(GraphicsErrc::OutOfVideoMemory);
-    }
-
-  id<MTLCommandBuffer>                       cmd = [dx.queue commandBuffer];
-  id<MTLAccelerationStructureCommandEncoder> enc = [cmd accelerationStructureCommandEncoder];
-  [enc buildAccelerationStructure:impl
-                       descriptor:desc
-                    scratchBuffer:scratch
-              scratchBufferOffset:0];
-  [enc endEncoding];
-  [cmd commit];
-  // TODO: implement proper upload engine
-  [cmd waitUntilCompleted];
 
   if(scratch!=nil)
     [scratch release];
