7b7f85b126cc8419f3a8cc63c3d7b6c4984fa58e
Try <try9998@gmail.com>
2026-09-10 00:31:39 +0200

    WA for VK_SUBOPTIMAL_KHR

--- Engine/gapi/vulkan/vswapchain.h
+++ Engine/gapi/vulkan/vswapchain.h
@@ -103,6 +103,7 @@ class VSwapchain : public AbstractGraphicsApi::Swapchain {
 
     VkFormat                 swapChainImageFormat = VK_FORMAT_UNDEFINED;
     VkExtent2D               swapChainExtent = {};
+    VkSurfaceCapabilitiesKHR swapChaincurrentCaps = {};
 
     void                     cleanupSwapchain() noexcept;
     void                     cleanupSurface() noexcept;
@@ -113,11 +114,12 @@ class VSwapchain : public AbstractGraphicsApi::Swapchain {
     VkResult                 createSwapchain(VDevice& device, const SwapChainSupport& support, const Rect& rect, uint32_t imgCount);
     void                     createImageViews(VDevice &device);
 
-    VkSurfaceFormatKHR       findSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-    VkPresentModeKHR         findSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
-    VkExtent2D               findSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities, uint32_t w, uint32_t h);
+    VkSurfaceFormatKHR       findSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) const;
+    VkPresentModeKHR         findSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) const;
+    VkExtent2D               findSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities, uint32_t w, uint32_t h) const;
     uint32_t                 findImageCount(const SwapChainSupport& support) const;
 
+    bool                     isSwapchainLost(VkResult code) const;
     VkResult                 implAcquireNextImage();
     void                     acquireNextImage();
   };
