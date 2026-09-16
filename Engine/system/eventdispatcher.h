6f1a67c301482f2cafc28e305aa4db179f393d07
Abendlied <abendlied@gmx.net>
2026-09-16 10:54:08 +0200

    Fix cursor helper parameter name

--- Engine/system/eventdispatcher.h
+++ Engine/system/eventdispatcher.h
@@ -40,7 +40,7 @@ class EventDispatcher final {
 
     bool                         implShortcut(Tempest::Widget &w, Tempest::KeyEvent& event);
     std::shared_ptr<Widget::Ref> implDispatch(Tempest::Widget &w, Tempest::KeyEvent&   event);
-    void                         implSetMouseOver(const std::shared_ptr<Widget::Ref>& s, MouseEvent& orig);
+    void                         implSetMouseOver(const std::shared_ptr<Widget::Ref>& wptr, MouseEvent& orig);
     void                         implExcMouseOver(Widget *w, Widget *old);
     void                         handleModKey(const KeyEvent& e);
 
